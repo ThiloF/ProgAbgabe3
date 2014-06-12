e119f31a73d6f6f2b1a1a65f17a5bd8a7c26a9a1
tfalk001 <tfalk001@pc01-102.intern.mi.hs-rm.de>
2014-06-12 12:29:18 +0200

    Fisch mit Konstruktor

--- ProgAbgabe3/src/Fisch.java
+++ ProgAbgabe3/src/Fisch.java
@@ -1,6 +1,12 @@
 
 public class Fisch implements Leckerbissen {
 
+	
+	public Fisch(String name, Nahrungstyp typ , Esstyp geschmack, int aktGewicht, int maxApetit) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 	@Override
 	public int getGramm() {
 		// TODO Auto-generated method stub
