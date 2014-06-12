2a4f1dabaed8f8c397fd591f337a780e4ec59238
fkoen001 <fkoen001@PC01-101.intern.mi.hs-rm.de>
2014-06-12 12:45:07 +0200

    Rechtschreibfehler behoben, vonName funktion

--- ProgAbgabe3/src/Esstyp.java
+++ ProgAbgabe3/src/Esstyp.java
@@ -1,6 +1,15 @@
 
 public enum Esstyp {
 
-	VEGETARIA, VEGANER, FLEXITARIA, FISCHESSER, FLEISCHESSER;
+	VEGETARIER, VEGANER, FLEXITARIER, FISCHFRESSER, FLEISCHFRESSER;
+	
+	public static Esstyp vonName(String s) {
+		if (s.equals("VEGETARIER")) return VEGETARIER;
+		if (s.equals("VEGANER")) return VEGANER;
+		if (s.equals("FLEXITARIER")) return FLEXITARIER;
+		if (s.equals("FISCHFRESSER")) return FISCHFRESSER;
+		if (s.equals("FLEISCHFRESSER")) return FLEISCHFRESSER;
+		return VEGETARIER;
+	}
 	
 }
