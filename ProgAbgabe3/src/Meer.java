cb84d475c87c90b2e640a51fa2835ac0dfb875bb
fkoen001 <fkoen001@PC13-104.intern.mi.hs-rm.de>
2014-06-13 16:37:12 +0200

    Mehr Inhalt, Geschichten werden schonmal gebaut

--- ProgAbgabe3/src/Meer.java
+++ ProgAbgabe3/src/Meer.java
@@ -1,23 +1,58 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Meer {
+
+	private ArrayList<Leckerbissen> inhalt = new ArrayList<Leckerbissen>();
+	private ArrayList<Fisch> fische = new ArrayList<Fisch>();
 
 	public Meer() {
 		ladeFische(new File("fische.txt"));
 		ladeZeug(new File("zeug.txt"));
-		for (Leckerbissen l : inhalt) {
-			System.out.println(l.toString() + l.getNahrungstyp());
+		/*
+		 * for (Leckerbissen l : inhalt) { System.out.println(l.toString() + l.getNahrungstyp()); }
+		 */
+
+		doStory();
+
+	}
+
+	private void doStory() {
+		Random random = new Random();
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File("out.txt")));
+			for (int i = 0; i < 1000; i++) {
+				Fisch fisch = fische.get(random.nextInt(fische.size()));
+				Leckerbissen leckerbissen = inhalt.get(random.nextInt(inhalt.size()));
+				try {
+					writer.write(fisch.getName() + ": ");
+					fisch.fressen(leckerbissen);
+					writer.write("hat gegessen: " + leckerbissen.toString());
+				} catch (MuellException | SchmecktNichtException | SattException | EatYourFriendException e) {
+					writer.write(e.getMessage());
+					e.printStackTrace();
+				}
+				writer.newLine();
+			}
+			writer.close();
+		} catch (IOException e) {
+			System.out.println("IO Error...");
+			e.printStackTrace();
 		}
 	}
 
-	private ArrayList<Leckerbissen> inhalt = new ArrayList<Leckerbissen>();
+	public void addFisch(Fisch fisch) {
+		fische.add(fisch);
+	}
 
 	public void addLeckerbissen(Leckerbissen leckerbissen) {
-		this.inhalt.add(leckerbissen);
+		inhalt.add(leckerbissen);
 	}
 
 	public void ladeFische(File fischdatei) {
@@ -48,7 +83,9 @@ public class Meer {
 					continue;
 				}
 
-				addLeckerbissen(new Fisch(parts[0].trim(), Nahrungstyp.vonName(parts[1].trim()), Esstyp.vonName(parts[2].trim()), gewicht, appetit));
+				Fisch fisch = new Fisch(parts[0].trim(), Nahrungstyp.vonName(parts[1].trim()), Esstyp.vonName(parts[2].trim()), gewicht, appetit);
+				addFisch(fisch);
+				addLeckerbissen(fisch);
 			}
 			reader.close();
 		} catch (IOException e) {
@@ -89,6 +126,11 @@ public class Meer {
 				for (int i = 0; i < anzahl; i++) {
 					addLeckerbissen(new Zeug(parts[0].trim(), Nahrungstyp.vonName(parts[1].trim()), gewicht));
 				}
+
+				// Zeug a = new Zeug("Stiefel", Nahrungstyp.MUELL, 100);
+				// Zeug b = new Zeug("Algen", Nahrungstyp.PFLANZE, 5);
+
+				// a.getGramm();
 
 			}
 			reader.close();
